c13e412729b0af0ce6fc73f9c15507d6b97013e4
GreatShadow <13421565@student.uts.edu.au>
2022-08-25 13:17:36 +0800

    Increase Models
    Increase Course Model and EnrolledCourse Model

--- students/models.py
+++ students/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    student_number = models.PositiveIntegerField()
+    student_number = models.PositiveIntegerField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
@@ -13,3 +13,12 @@ class Student(models.Model):
 
     def __str__(self):
         return f'Student: {self.first_name} {self.last_name}'
+
+class Course(models.Model):
+    course_number = models.PositiveIntegerField(unique=True)
+    course_name = models.CharField(max_length=100)
+    credit = models.PositiveIntegerField()
+
+class EnrolledCourse(models.Model):
+    student_number = models.ForeignKey(to="Student", to_field="student_number", on_delete=models.CASCADE)
+    course_number = models.ForeignKey(to="Course", to_field="course_number", on_delete=models.CASCADE)
