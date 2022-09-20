fb1a6928a76d449d5707204718556c5e80df63ff
Fairchild2333 <79494757+Fairchild2333@users.noreply.github.com>
2022-09-21 00:04:25 +1000

    test

--- SCRS/settings.py
+++ SCRS/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'students.middleware.Middleware'
+    #'students.middleware.Middleware'
 ]
 
 ROOT_URLCONF = 'SCRS.urls'
